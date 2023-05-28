292269cb0bd4a59fa316a9a0e90bb32c7e3f9bc7
Alfa <210711378@students.uajy.ac.id>
2023-05-28 23:29:56 +0700

    first commit

---
+++ GD12_A_11378_1/src/gd12_a_11378_1/GD12_A_11378_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gd12_a_11378_1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class GD12_A_11378_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ GD12_A_11378_1/src/gd12_a_11378_1/Kepala.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package gd12_a_11378_1;
+
+/**
+ *
+ * @author Alfa
+ */
+public class Kepala {
+
+}
